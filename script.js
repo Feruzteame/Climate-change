86ed41a648982be42c8743c2578c753524ff94e9
feruzteame <teamefitwi@gmail.com>
2020-05-20 13:54:08 +0200

    html-css-j.script

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function display() {
+  var about = document.getElementById("textAbout");
+  if (about.style.display === "none") {
+    about.style.display = "block";
+  } else{
+      about.style.display ="block";
+  }
+}
